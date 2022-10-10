53fcca31403562a97ac877b3084223d333874f85
mrbrist <bristol2801@gmail.com>
2022-10-10 18:43:48 +0100

    Super basic player movement
    Will need to tweak some things tomorrow

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed;
+    public float jumpForce;
+    private float moveInput;
+
+    private Rigidbody2D rb;
+    public Transform feet;
+    public LayerMask groundLayers;
+
+    //start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        moveInput = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && isGrounded())
+        {
+            Jump();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 movement = new Vector2(moveInput * speed, rb.velocity.y);
+
+        rb.velocity = movement;
+    }
+
+    void Jump()
+    {
+        Vector2 movement = new Vector2(rb.velocity.x, jumpForce);
+
+        rb.velocity = movement;
+    }
+
+    bool isGrounded()
+    {
+        Collider2D groundCheck = Physics2D.OverlapCircle(feet.position, 0.5f, groundLayers);
+
+        if (groundCheck != null)
+        {
+            return true;
+        }
+
+        return false;
     }
 }
