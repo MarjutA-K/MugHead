b9a6215f1efbc5e0a0250c05a2c6ad9e258f6181
LV <Loo3.Wong@live.uwe.ac.uk>
2022-10-17 20:01:40 +0100

    Boomerang Concept Done. (Without Having Boomerang Sprite Sheets)

--- Assets/Scripts/Attack3Behaviour/Attack3Behaviour.cs
+++ Assets/Scripts/Attack3Behaviour/Attack3Behaviour.cs
@@ -7,6 +7,7 @@ public class Attack3Behaviour : MonoBehaviour
     public float speed = 10;
     public Rigidbody2D rb;
     public GameObject Player;
+    public GameObject Boss;
 
     float target;
     Vector2 Direction;
@@ -15,6 +16,7 @@ public class Attack3Behaviour : MonoBehaviour
     void Start()
     {
         Player = GameObject.Find("Player");
+        Boss = GameObject.Find("Boss");
         Direction = (Player.transform.position - transform.position).normalized * speed;
         rb.velocity = new Vector2(Direction.x, 0f) ;
     }
@@ -23,10 +25,16 @@ public class Attack3Behaviour : MonoBehaviour
     {
         if(collision.CompareTag("Player"))
         {
+            Player.GetComponent<PlayerController>().PlayerDamage(Boss.GetComponent<EnemyBehaviour>().EnemyBulletDamage);
             Destroy(gameObject);
         }
 
         if(collision.CompareTag("CollisionEnd"))
+        {
+            rb.velocity = transform.right * speed;
+        }
+
+        if(collision.CompareTag("CollisionStart"))
         {
             Destroy(gameObject);
         }
